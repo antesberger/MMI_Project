cb25b37a530f5ee3622aea688cf0d39a93cd80fc
Lukas Antesberger <lukas@antesberger.net>
2017-03-24 15:16:03 +0100

    working version

--- server.php
+++ server.php
@@ -3,35 +3,32 @@
   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
   <?php
+    
     if(isset($_POST['x'],$_POST['y'],$_POST['direction'],$_POST['time'],$_POST['volume'],$_POST['uid'])){
-
       //update
-      // $db = mysqli_connect('127.0.0.3', 'db388648_1', 'MMMIinfo', 'db388648_1');
-      // if ($db->connect_error) {
-      //   die('Failed to connect: '.$db->connect_error);
-      // }
+      $db = mysqli_connect('127.0.0.3', 'db388648_1', 'MMMIinfo', 'db388648_1');
+      if ($db->connect_error) {
+        die('Failed to connect: '.$db->connect_error);
+      }
 
-      // $query = "INSERT INTO TestExperiments VALUES (
-      // NULL,
-      // " . $_POST['x'] . ", 
-      // " . $_POST['y'] . ",
-      // \"" . $_POST['direction'] . "\",
-      // " . $_POST['time'] . ",
-      // " . $_POST['volume'] . ", 
-      // NULL,
-      // \"" . $_POST['uid'] . "\")";
+      $query = "INSERT INTO TestExperiments VALUES (
+      NULL,
+      " . $_POST['x'] . ", 
+      " . $_POST['y'] . ",
+      \"" . $_POST['direction'] . "\",
+      " . $_POST['time'] . ",
+      " . $_POST['volume'] . ", 
+      NULL,
+      \"" . $_POST['uid'] . "\")";
 
-      // $result = $db->query($query);
+      $result = $db->query($query);
 
-      // if (!$result) {
-      //   die('INSERT failed: '.$db->error);
-      // } 
+      if (!$result) {
+        die('INSERT failed: '.$db->error);
+      } 
 
-      // $db->close();
-    } else {
-      //insert row
+      $db->close();
     }
   ?>
 </body>
